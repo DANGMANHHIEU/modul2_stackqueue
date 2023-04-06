9042e5662f381c8be93e5665eb9a78dd3a2b6c87
DANGMANHHIEU <manhhieu261296@gmail.com>
2023-04-06 10:00:11 +0700

    doc chuoi nguoc xuoi

---
+++ src/checkqueue/test.java
@@ -0,0 +1,10 @@
+package checkqueue;
+
+public class test {
+    public static void main(String[] args) {
+        String s1 ="able was I ere I saw ElbA";
+        String s2 = "Hello world";
+        System.out.println( check.checkString(s1));
+        System.out.println(check.checkString(s2));
+    }
+}
